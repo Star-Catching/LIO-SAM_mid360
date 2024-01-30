deb0544edd3fc8b1168cc00c711f3e9f9fed0871
Star-Catching <1612627719@qq.com>
2024-01-30 15:33:34 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/wxz/LIO_SAM_ws/src/LIO-SAM
+# Build directory: /home/wxz/LIO_SAM_ws/src/LIO-SAM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
